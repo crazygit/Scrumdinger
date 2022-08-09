84403c20e973881f816abbac18c50319e12ba41c
crazygit <lianglin999@gmail.com>
2022-08-09 15:46:12 +0800

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Crazygit on 2022/8/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Crazygit on 2022/8/9.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
