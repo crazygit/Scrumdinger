dcf661eb6983e6022043e3fe0c02a78c8d2b3686
crazygit <lianglin999@gmail.com>
2022-08-11 16:16:36 +0800

    Navigation and Modal Presentation

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,59 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Crazygit on 2022/8/11.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    @State private var data = DailyScrum.Data()
+    // The newAttendeeName property will hold the attendee name that the user enters.
+    @State private var newAttendeeName = ""
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $data.title)
+                HStack {
+                    Slider(value: $data.lengthInMinutes, in: 5 ... 30, step: 1) {
+                        Text("Length")
+                    }
+                    .accessibilityValue("\(Int(data.lengthInMinutes)) minutes")
+                    Spacer()
+                    Text("\(Int(data.lengthInMinutes)) minutes")
+                        .accessibilityHidden(true)
+                }
+            }
+            Section(header: Text("Attendees")) {
+                ForEach(data.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indices in
+                    data.attendees.remove(atOffsets: indices)
+                }
+
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            data.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
--- Scrumdinger/Models/DailyScrum.swift
+++ Scrumdinger/Models/DailyScrum.swift
@@ -7,11 +7,45 @@
 
 import Foundation
 
-struct DailyScrum {
+struct DailyScrum: Identifiable {
+    let id: UUID
     var title: String
-    var attendees: [String]
+    var attendees: [Attendee]
     var lengthInMinutes: Int
     var theme: Theme
+
+    init(id: UUID = UUID(), title: String, attendees: [String], lengthInMinutes: Int, theme: Theme) {
+        self.id = id
+        self.title = title
+        self.attendees = attendees.map { Attendee(name: $0) }
+        self.lengthInMinutes = lengthInMinutes
+        self.theme = theme
+    }
+}
+
+extension DailyScrum {
+    struct Attendee: Identifiable {
+        let id: UUID
+        var name: String
+
+        init(id: UUID = UUID(), name: String) {
+            self.id = id
+            self.name = name
+        }
+    }
+    
+    // Data will contain all the editable properties of DailyScrum.
+    // The types of the properties match the controls that you’ll create in the edit view.
+    struct Data {
+        var title: String = ""
+        var attendees: [Attendee] = []
+        var lengthInMinutes: Double = 5
+        var theme: Theme = .seafoam
+    }
+    
+    var data:Data {
+        return Data(title: title, attendees: attendees, lengthInMinutes: Double(lengthInMinutes), theme: theme)
+    }
 }
 
 // 利用扩展添加一些测试数据，后期也可以方便地删除
